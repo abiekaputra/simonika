1692c5221236c3c817f9e314f54a825d24976a71
Abi Eka Putra Wulyono <abiekapw@gmail.com>
2026-06-08 09:51:13 +0700

    PenggunaController: remove boilerplate docblocks

--- app/Http/Controllers/PenggunaController.php
+++ app/Http/Controllers/PenggunaController.php
@@ -7,59 +7,11 @@ use Illuminate\Http\Request;
 
 class PenggunaController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(Pengguna $pengguna)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Pengguna $pengguna)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Pengguna $pengguna)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Pengguna $pengguna)
-    {
-        //
-    }
+    public function index() {}
+    public function create() {}
+    public function store(Request $request) {}
+    public function show(Pengguna $pengguna) {}
+    public function edit(Pengguna $pengguna) {}
+    public function update(Request $request, Pengguna $pengguna) {}
+    public function destroy(Pengguna $pengguna) {}
 }
